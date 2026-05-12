7fe17a26aa9d230999923187d0169e93b9f96936
Alexander <axmitrofanov@gmail.com>
2026-05-12 17:18:38 +0300

    update

--- install.sh
+++ install.sh
@@ -165,20 +165,6 @@ get_env() {
   fi
 }
 
-remove_env_keys() {
-  local tmp
-
-  if [[ ! -f "$ENV_FILE" ]]; then
-    return 0
-  fi
-
-  tmp="$(mktemp)"
-
-  grep -Ev '^(TELEGRAM_ENABLED|TELEGRAM_BOT_TOKEN|TELEGRAM_ADMIN_ID|TELEGRAM_CHAT_ID|TELEGRAM_TOPIC_ID)=' "$ENV_FILE" > "$tmp" || true
-  mv "$tmp" "$ENV_FILE"
-  chmod 600 "$ENV_FILE"
-}
-
 load_env() {
   set -a
   source "$ENV_FILE"
@@ -453,7 +439,6 @@ rm -f "${APP_DIR}/menu"
 if [[ "$HAS_EXISTING_ENV" == "false" ]]; then
   write_env_file
 else
-  remove_env_keys
   set_env "INSTANCE_NAME" "$INSTANCE_NAME"
   set_env "SERVICE_UNIT" "$SERVICE_UNIT"
   set_env "TIMER_UNIT" "$TIMER_UNIT"
